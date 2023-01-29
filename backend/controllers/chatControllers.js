4ea3d380ccd25b8d3f9912300c25f73b6d5c3a43
Anandha Padmanabhan K.U <93422905+anandhapadmanabhan58@users.noreply.github.com>
2023-01-29 18:17:32 +0530

    ongoing with groups

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -49,4 +49,60 @@ const accessChat = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = accessChat;
+const fetchChats = asyncHandler(async (req, res, next) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate('users', '-password')
+      .populate('groupAdmin')
+      .populate('latestMessage')
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: 'latestMessage.sender',
+          select: 'name email pic',
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const createGroupChat = asyncHandler(async (req, res, next) => {
+  if (!req.body.users || !req.body.name) {
+    res.status(400).send({
+      message: 'fill the details',
+    });
+  }
+
+  let users = JSON.parse(req.body.users);
+
+  console.log(users);
+  if (users.length < 2) {
+    res.send(400).send({ message: 'more than 2 users required' });
+  }
+
+  users.push(req.user._id);
+  console.log(users);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user._id,
+    });
+
+    const fetchedGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password');
+
+    res.status(200).json(fetchedGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { checkJWT } = require('../middlewares/auth');
-const accessChat = require('../controllers/chatControllers');
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+} = require('../controllers/chatControllers');
 const router = express.Router();
 
-router.route('/').post(checkJWT, accessChat);
-// router.route('/').get(checkJWT, fetchChats);
-// router.route('/group').post(checkJWT, createGroup);
+router.route('/').post(checkJWT, accessChat).get(checkJWT, fetchChats);
+router.route('/group').post(checkJWT, createGroupChat);
 // router.route('/rename').put(checkJWT, renameGroup);
 // router.route('/removegroup').put(checkJWT, removeFromGroup);
 // router.route('/groupadd').put(checkJWT, addToGroup);
